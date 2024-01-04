6ab72e58acf1f7defe722825e64ad939bf8a3a1a
selenayack <slny_ackgoz@hotmail.com>
2024-01-04 17:54:52 +0300

    log eklendi

--- app/src/main/java/com/example/fitbuddy/RegisterActivity.java
+++ app/src/main/java/com/example/fitbuddy/RegisterActivity.java
@@ -7,6 +7,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
