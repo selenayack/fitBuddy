7c2accf2baa2137c440c1e58dc8906be7dd0bd6f
selenayack <slny_ackgoz@hotmail.com>
2024-01-04 17:44:21 +0300

    anasayfa tasarımına başlandı,veritabanı eklendi

--- app/src/main/java/com/example/fitbuddy/HomeFragment.java
+++ app/src/main/java/com/example/fitbuddy/HomeFragment.java
@@ -1,35 +1,71 @@
 package com.example.fitbuddy;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-
-import java.util.ArrayList;
-import java.util.List;
-
 
 public class HomeFragment extends Fragment {
 
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        LinearLayout linearLayout1 = view.findViewById(R.id.linearlayout); // Bu kısmı kendi layoutunuzdaki LinearLayout'a uygun ID ile değiştirin
+
+        // Tıklama olayını dinlemek için onClickListener ekleyin
+        linearLayout1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Yeni bir activity başlatmak için Intent kullanarak startActivity metodunu çağırın
+                Intent intent = new Intent(getActivity(), user_infos.class); // YourNewActivity yerine açmak istediğiniz Activity'nin adını yazın
+                startActivity(intent);
+            }
+        });;
+
+
+
+        Context context = getContext(); // Context'i uygun şekilde alın
+        String dbPath = context.getDatabasePath("fitBuddyDiet.db").getAbsolutePath();
+        Log.d("DatabasePath", "Veritabanı Yolu: " + dbPath);
+
+        // Database işlemleri burada gerçekleştirilebilir
+        DbAdapter db = new DbAdapter(requireContext());
+        db.open();
+
+        int rows=db.count("food");
+
+        if(rows<1){
+            Toast.makeText(requireContext(), "Yükleniyor", Toast.LENGTH_SHORT).show();
+            DBSetupInsert setupInsert=new DBSetupInsert(requireContext());
+            setupInsert.insertAllFood();
+            setupInsert.insertAllCategories();
+            Toast.makeText(requireContext(), "Yüklendi", Toast.LENGTH_SHORT).show();
+
+
+
+        }
+
+
+
+        //db.close();
+
+
+
+
+
+
+
+
+
+        return view;
     }
-
-
 }
